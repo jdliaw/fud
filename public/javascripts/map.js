804ec36b16a1aea270c2d0108fd1d863824cf42c
Brandon Liu <bliu7654@gmail.com>
2016-07-24 12:41:44 -0400

    Changed placeholder text to actually stuff. Updated map.js so that it's a FeatureCollection (holds more than 1 feature now) and thus added Rigoberto's.

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,23 +1,48 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamRsaWF3IiwiYSI6ImNpcjAzZHdsMzAycjVmc2txZHp6M2JtOHEifQ.S03POBe5nKC1CDRnJANxdw';
   var map = L.mapbox.map('map', 'mapbox.streets')
-      .setView([32.630395, -117.093245], 10);
+    .setView([32.630395, -117.093245], 10);
 
 myLayer = L.mapbox.featureLayer({
-    type: 'Feature',
-    geometry: {
+  type: 'FeatureCollection',
+  features: [ 
+    {
+      type: 'Feature',
+      geometry: {
         type: 'Point',
         coordinates: [
           -117.093245,
           32.630395
         ]
+      },
+      properties: {
+          title: 'Tacos El Gordo',
+          description: '689 H Street, Chula Vista, CA 91910',
+          'marker-size': 'large',
+          'marker-color': '#f75850',
+          'marker-symbol': 'restaurant'
+      }
     },
-    properties: {
-        title: 'Tacos El Gordo',
-        description: '689 H Street, Chula Vista, CA 91910',
-        'marker-size': 'large',
-        'marker-color': '#f75850',
-        'marker-symbol': 'restaurant'
-    }
+
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          -117.162116,
+          32.87998
+        ]
+      },
+      properties: {
+          title: "Rigoberto's Taco Shop",
+          description: '7094 Miramar Rd, San Diego, CA 92121',
+          'marker-size': 'large',
+          'marker-color': '#f75850',
+          'marker-symbol': 'restaurant'
+      }
+    },
+  ]
+
+
 }).addTo(map);
 
 var info = document.getElementById('info');
