bc406a33a1b842a2e861ff5e4f43ee420595dbee
jdliaw <jennifer.liaw07@gmail.com>
2016-07-23 22:47:36 -0700

    tacos el gordo baby

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,3 +1,21 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamRsaWF3IiwiYSI6ImNpcjAzZHdsMzAycjVmc2txZHp6M2JtOHEifQ.S03POBe5nKC1CDRnJANxdw';
   var map = L.mapbox.map('map', 'mapbox.streets')
-      .setView([40, -74.50], 9);
+      .setView([32.630395, -117.093245], 10);
+
+L.mapbox.featureLayer({
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        coordinates: [
+          -117.093245,
+          32.630395
+        ]
+    },
+    properties: {
+        title: 'Tacos El Gordo',
+        description: '689 H Street, Chula Vista, CA 91910',
+        'marker-size': 'large',
+        'marker-color': '#f75850',
+        'marker-symbol': 'restaurant'
+    }
+}).addTo(map);
