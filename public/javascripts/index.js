f93143c21cdad035a71598675ebb6ed7868db27e
jdliaw <jennifer.liaw07@gmail.com>
2016-07-24 01:40:54 -0700

    temp splash page

---
+++ public/javascripts/index.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiamRsaWF3IiwiYSI6ImNpcjAzZHdsMzAycjVmc2txZHp6M2JtOHEifQ.S03POBe5nKC1CDRnJANxdw';
+  var bigMap = L.mapbox.map('big-map', 'mapbox.streets')
+      .setView([32.630395, -117.093245], 2);
