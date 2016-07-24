4b5c983fe15957d0e69cc4cce4de98000e892aa1
jdliaw <jennifer.liaw07@gmail.com>
2016-07-23 21:57:57 -0700

    got a simple map in yay mapbox

---
+++ public/javascripts/map.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiamRsaWF3IiwiYSI6ImNpcjAzZHdsMzAycjVmc2txZHp6M2JtOHEifQ.S03POBe5nKC1CDRnJANxdw';
+  var map = L.mapbox.map('map', 'mapbox.streets')
+      .setView([40, -74.50], 9);
