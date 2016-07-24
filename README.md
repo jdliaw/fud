4ea239ae2767d3a693737f93340a3b55e7386495
jdliaw <jennifer.liaw07@gmail.com>
2016-07-23 20:31:16 -0700

    we have sass wooooo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fud
 we like füd
+
+# Usage
+Run `npm start` and see the magic at [localhost:3000](http://127.0.0.1:3000)
+
+# License
+MIT [The MIT License.](https://opensource.org/licenses/MIT)
--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var sass = require('node-sass');
+var sassMiddleware = require('node-sass-middleware');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,6 +23,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// adding the sass middleware
+app.use(
+   sassMiddleware({
+       src: __dirname + '/sass',
+       dest: __dirname + '/public/stylesheets',
+       prefix:  '/stylesheets',
+       debug: true,
+   })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
