a2e76d7d36bb5d0053e42e16410bd5ec633fe187
jdliaw <jennifer.liaw07@gmail.com>
2016-07-24 01:46:16 -0700

    static bg

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,8 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamRsaWF3IiwiYSI6ImNpcjAzZHdsMzAycjVmc2txZHp6M2JtOHEifQ.S03POBe5nKC1CDRnJANxdw';
-  var bigMap = L.mapbox.map('big-map', 'mapbox.streets')
+  var bigMap = L.mapbox.map('big-map', 'mapbox.streets', {zoomControl: false})
       .setView([32.630395, -117.093245], 2);
+
+bigMap.dragging.disable();
+bigMap.touchZoom.disable();
+bigMap.doubleClickZoom.disable();
+bigMap.scrollWheelZoom.disable();
