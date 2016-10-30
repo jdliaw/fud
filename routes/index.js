d0c8a4de8899df3e9ef12f2d4302619c0e797a22
Brandon Liu <bliu7654@gmail.com>
2016-10-29 19:44:40 -0700

    Got mongo up and running

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,52 @@
-var express = require('express'); 
-var path = require('path'); 
-var app = express();   
+var express = require('express');
+var path = require('path');
+var app = express();
 
-var bodyParser = require('body-parser');  
-app.use(bodyParser.json());  
+var mongodb = require('mongodb');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var router = express.Router();  
+var router = express.Router();
+app.use(router);
 
-app.use(router);  
-
+/* mongodb stuff.......... */
+var uri = 'mongodb://test:test@ds011705.mlab.com:11705/fud_map';
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/main', function(req, res, next) {
+router.get('/main', function (req, res, next) {
   res.render('main');
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   res.render('about');
 });
 
-router.get('/add', function(req, res, next) {
+router.get('/add', function (req, res, next) {
   res.render('add');
 });
 
-router.post('/add/restaurant', function(req, res, next) {
+router.post('/add/restaurant', function (req, res, next) {
   console.log("Output of req.body from POST: in parts:")
-  
+
   //req.body is the JSON object that looks like a string.
   console.log(req.body);
-  var isFilledOut = true;  
+  var isFilledOut = true;
   //Loops through req.body and use req.body[key] to access the input.
-  for(var key in req.body) {
+  for (var key in req.body) {
     var value = req.body[key];
     //null or empty string
-    if(!value || value === "") {
+    if (!value || value === "") {
       isFilledOut = false;
     }
     var formattedReview;
-    if(key === "review") {
+    if (key === "review") {
       var rawReview = req.body[key];
       var dealWithNewLines = rawReview.replace(/\r\n/g, "\n");
       var convertToPTags = dealWithNewLines.split("\n").join("</p><p>");
@@ -51,8 +54,37 @@ router.post('/add/restaurant', function(req, res, next) {
       req.body.review = formattedReview;
     }
   }
+
   console.log(req.body);
-  if(isFilledOut) {
+  //if formatted correctly.... add to database
+  if (isFilledOut) {
+    mongodb.MongoClient.connect(uri, function (err, db) {
+      if (err) {
+        throw err;
+      }
+      /*
+       * First we'll add a few songs. Nothing is required to create the 
+       * songs collection; it is created automatically when we insert.
+       */
+      var restaurants = db.collection('restaurants');
+      // Note that the insert method can take either an array or a dict.
+
+      restaurants.insert(req.body, function (err, result) {
+        if (err) {
+          throw err;
+        }
+        // restaurants.update(
+        //   { title: req.body.title },
+        //   { address: req.body.address},
+        //   { category: req.body.category},
+        //   { priceinput: req.body.price-input},
+        //   { ratinginput: req.body.price-input},
+        //   { review: req.body.review},
+        // );
+      });
+    });
+
+
     res.send("Thanks for submitting! Click <a href='/'>here</a> to go back.");
   }
 });
